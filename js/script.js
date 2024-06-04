8d671d30011d2bef47469fa09c768b8d487bde83
devrimpa <166713901+devrimpa@users.noreply.github.com>
2024-06-04 23:52:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$('.slide').slick({
+    dots: false,
+    arrows: true,
+    fade: false,
+    // autoplay: true,
+    autoplaySpeed: 4000,
+});
+
+
+$('.experience-section').slick({
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+});
+
+
+
+
+// go to the top
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
